797deb12eb0b2c43832c38d1a1d505cfcee9de9b
harizmdp <harizmdp619@gmail.com>
2026-04-19 13:58:01 +0530

    address review commands

--- LedBlink.c
+++ LedBlink.c
@@ -4,9 +4,9 @@ int main()
 {
     while(1)
     {
-        LED1 = 0xff; // Turn on LED1
+        LED1 = 0xfe; // Turn on LED1
         delay(1000); // Delay for 1 second
-        LED1 = 9; // Turn off LED1
+        LED1 = 0x01; // Turn off LED1
         delay(1000); // Delay for 1 second
     }
 }
